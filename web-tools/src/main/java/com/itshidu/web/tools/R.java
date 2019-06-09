814cffd43038c8ac31d9282589683c9ce9d9166f
XiaZengming <1500506007@qq.com>
2019-06-09 11:42:29 +0800

    Result改名为R

--- web-tools/src/main/java/com/itshidu/web/tools/R.java
+++ web-tools/src/main/java/com/itshidu/web/tools/R.java
@@ -1,5 +1,34 @@
 package com.itshidu.web.tools;
 
-public class R extends Result {
+import java.util.HashMap;
 
+public class R extends HashMap<String, Object> {
+	
+	public static R of() {
+		return new R();
+	}
+	public static R of(int code) {
+		R er = new R();
+		er.put("code", code);
+		return er;
+	}
+	public static R of(int code,String msg) {
+		R er = new R();
+		er.put("code", code);
+		er.put("msg", msg);
+		return er;
+	}
+	public static R of(int code,String msg,Object data) {
+		R er = new R();
+		er.put("code", code);
+		er.put("msg", msg);
+		er.put("data", data);
+		return er;
+	}
+	
+	public R put(String key,Object value) {
+		super.put(key, value);
+		return this;
+	}
+	
 }
